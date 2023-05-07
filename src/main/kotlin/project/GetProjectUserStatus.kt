525851f0d05db6001a270f92e0f363e8c352f8dd
Jeff <jeff@weltcorp.com>
2023-05-07 21:48:12 +0900

    [#21] fix: core.getProjectUserStatus() - UNIMPLEMENTED error

--- src/main/kotlin/project/GetProjectUserStatus.kt
+++ src/main/kotlin/project/GetProjectUserStatus.kt
@@ -6,13 +6,13 @@ import com.weltcorp.dtx.core.lib.project.datasource.ProjectRemoteDataSourceGrpcI
 suspend fun main(args: Array<String>) {
     val config = ProjectApiConfig.Builder()
         .host("dtx-api-dev.weltcorp.com")
-        .port(443) // The port can be removed, if you don't need.
+//        .port(443) // The port can be removed, if you don't need.
         .auth("<YOUR-TOKEN>")
         .build()
 
     val projectRemoteDataSource = ProjectRemoteDataSourceGrpcImpl(config)
 
-    val userId = 100
+    val userId = 17933
     val res = projectRemoteDataSource.getProjectUserStatus(userId)
     println(res)
 }
